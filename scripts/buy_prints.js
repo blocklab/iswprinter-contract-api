09f38e1f981643c862bc001be1cfe33d9b023051
Axel Hodler <axel@hodler.co>
2017-08-22 12:46:59 +0200

    script to buy access to printable

--- scripts/buy_prints.js
+++ scripts/buy_prints.js
@@ -4,4 +4,17 @@ const web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8545'));
 
 const printerAddress = contractProperties.networks[Object.keys(contractProperties.networks)[0]].address
 
-var contract = new web3.eth.Contract(contractProperties.abi, printerAddress)
+const contract = new web3.eth.Contract(contractProperties.abi, printerAddress)
+
+const gCodeHash = web3.utils.sha3('somegcode')
+
+async function run() {
+  const accounts = await web3.eth.getAccounts()
+  await contract.methods.buyRightToPrintOnce(gCodeHash).send({
+    from: accounts[1],
+    value: 10000
+  })
+  console.log('Bought ', gCodeHash, ' using Account: ' + accounts[1])
+}
+
+run()
