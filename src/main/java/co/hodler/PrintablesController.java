156bd42155cbea14d4c1365765195e951ab4a255
Axel Hodler <axel@hodler.co>
2017-08-22 08:41:56 +0200

    else class using Validated only null properties

--- src/main/java/co/hodler/PrintablesController.java
+++ src/main/java/co/hodler/PrintablesController.java
@@ -7,8 +7,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.beanvalidation
+  .MethodValidationPostProcessor;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -42,6 +45,11 @@ public class PrintablesController {
                               @PathVariable("userId") String userId) {
     contractService.resetPrints(new PrintableId(printableId), new UserId
       (userId));
+  }
+
+  @Bean
+  MethodValidationPostProcessor methodValidationPostProcessor() {
+    return new MethodValidationPostProcessor();
   }
 
   public static void main(String[] args) throws Exception {
