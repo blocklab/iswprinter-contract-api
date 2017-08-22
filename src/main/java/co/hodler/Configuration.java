6fa0a978b6541caffd9004005c7690cc3f629a6c
Axel Hodler <axel@hodler.co>
2017-08-22 07:56:53 +0200

    validate configuration during start

--- src/main/java/co/hodler/Configuration.java
+++ src/main/java/co/hodler/Configuration.java
@@ -1,14 +1,27 @@
 package co.hodler;
 
+import org.hibernate.validator.constraints.NotEmpty;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
+import org.springframework.validation.annotation.Validated;
+
+import javax.validation.constraints.Size;
 
 @Component
+@Validated
+@ConfigurationProperties
 public class Configuration {
+
+  @Size(min = 42, max = 42)
   @Value("${contractAddress}")
   public String contractAddress;
+
+  @NotEmpty
   @Value("${walletSource}")
   public String walletSource;
+
+  @NotEmpty
   @Value("${walletPassword}")
   public String walletPassword;
 }
